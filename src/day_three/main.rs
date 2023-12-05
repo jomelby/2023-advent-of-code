d19e90ee45b0db4252d50b95289867188f3d98fa
Joel Melby <jmelby@treeline.bio>
2023-12-05 08:17:07 -0500

    day three: part 2 stuck

--- src/day_three/main.rs
+++ src/day_three/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use advent2023::advent_utils::get_lines_from_filepath;
 
 fn get_surrounding_squares(row_idx: usize, col_idx: usize, matrix: &Vec<Vec<char>>) -> Vec<char> {
@@ -54,7 +56,7 @@ fn check_surrounding_squares_for_non_numeric_non_period_chars(
 // need to get 123 from row_idx of 2, 3, and 4
 fn get_full_number_from_row_idx(row_idx: usize, row: &Vec<char>) -> i32 {
     // need to check to the left and to the right of the idx for numeric characters
-    let mut full_number: i32 = 0;
+    // let mut full_number: i32 = 0;
     let mut number_string: String = String::new();
     for char in row[0..row_idx + 1].iter().rev() {
         if char.is_numeric() {
@@ -72,11 +74,11 @@ fn get_full_number_from_row_idx(row_idx: usize, row: &Vec<char>) -> i32 {
             break;
         }
     }
-    full_number = number_string.parse::<i32>().unwrap();
+    let full_number = number_string.parse::<i32>().unwrap();
     full_number
 }
 
-fn main() {
+fn part_one() {
     let lines: Vec<String> = get_lines_from_filepath("data/day_three_input.txt");
     let mut matrix: Vec<Vec<char>> = Vec::new();
     for line in lines {
@@ -115,4 +117,139 @@ fn main() {
     }
     let _sum: i32 = sum_array.iter().sum();
     println!("Sum array: {}", _sum);
+}
+
+fn get_surrounding_numeric_coordinates(
+    row_idx: usize,
+    col_idx: usize,
+    matrix: &Vec<Vec<char>>,
+) -> Vec<Vec<usize>> {
+    let mut surrounding_numeric_coordinates: Vec<Vec<usize>> = Vec::new();
+    // check the square above
+    if row_idx > 0 {
+        if matrix[row_idx - 1][col_idx].is_numeric() {
+            surrounding_numeric_coordinates.push(vec![row_idx - 1, col_idx]);
+        }
+    }
+    // check the square below
+    if row_idx < matrix.len() - 1 {
+        if matrix[row_idx + 1][col_idx].is_numeric() {
+            surrounding_numeric_coordinates.push(vec![row_idx + 1, col_idx]);
+        }
+    }
+    // check the square to the left
+    if col_idx > 0 {
+        if matrix[row_idx][col_idx - 1].is_numeric() {
+            surrounding_numeric_coordinates.push(vec![row_idx, col_idx - 1]);
+        }
+    }
+    // check the square to the right
+    if col_idx < matrix[0].len() - 1 {
+        if matrix[row_idx][col_idx + 1].is_numeric() {
+            surrounding_numeric_coordinates.push(vec![row_idx, col_idx + 1]);
+        }
+    }
+    // check the square above and to the left
+    if row_idx > 0 && col_idx > 0 {
+        if matrix[row_idx - 1][col_idx - 1].is_numeric() {
+            surrounding_numeric_coordinates.push(vec![row_idx - 1, col_idx - 1]);
+        }
+    }
+    // check the square above and to the right
+    if row_idx > 0 && col_idx < matrix[0].len() - 1 {
+        if matrix[row_idx - 1][col_idx + 1].is_numeric() {
+            surrounding_numeric_coordinates.push(vec![row_idx - 1, col_idx + 1]);
+        }
+    }
+    // check the square below and to the left
+    if row_idx < matrix.len() - 1 && col_idx > 0 {
+        if matrix[row_idx + 1][col_idx - 1].is_numeric() {
+            surrounding_numeric_coordinates.push(vec![row_idx + 1, col_idx - 1]);
+        }
+    }
+    // check the square below and to the right
+    if row_idx < matrix.len() - 1 && col_idx < matrix[0].len() - 1 {
+        if matrix[row_idx + 1][col_idx + 1].is_numeric() {
+            surrounding_numeric_coordinates.push(vec![row_idx + 1, col_idx + 1]);
+        }
+    }
+    surrounding_numeric_coordinates
+}
+
+fn get_full_numbers_around_coordinates(
+    row_idx: usize,
+    col_idx: usize,
+    matrix: &Vec<Vec<char>>,
+) -> Vec<i128> {
+    let mut full_numbers: Vec<i128> = Vec::new();
+    for surrounding_numeric_coordinate in
+        get_surrounding_numeric_coordinates(row_idx, col_idx, matrix)
+    {
+        let full_number = get_full_number_from_row_idx(
+            surrounding_numeric_coordinate[1],
+            &matrix[surrounding_numeric_coordinate[0]],
+        );
+        full_numbers.push(full_number as i128);
+    }
+    full_numbers
+}
+
+fn part_two() {
+    let lines: Vec<String> = get_lines_from_filepath("data/day_three_input.txt");
+    let mut matrix: Vec<Vec<char>> = Vec::new();
+    for line in lines {
+        let mut row: Vec<char> = Vec::new();
+        for c in line.chars() {
+            row.push(c);
+        }
+        matrix.push(row);
+    }
+    println!("Number of rows: {}", matrix.len());
+    println!("Number of columns: {}", matrix[0].len());
+    // println!("{:?}", matrix);
+    let mut sum_array: Vec<i128> = Vec::new();
+    let mut sum_value: i128 = 0;
+    for (row_idx, row) in matrix.iter().enumerate() {
+        for (col_idx, col) in row.iter().enumerate() {
+            if *col == '*' {
+                // check surround squares for two numbers
+                let surrounding_chars: Vec<char> =
+                    get_surrounding_squares(row_idx, col_idx, &matrix);
+                // println!("Found {:?}", surrounding_chars);
+                let mut surrounding_numbers: Vec<i128> = Vec::new();
+                for surrounding_char in surrounding_chars {
+                    if surrounding_char.is_numeric() {
+                        surrounding_numbers.push(surrounding_char.to_digit(10).unwrap() as i128);
+                    }
+                }
+                if surrounding_numbers.len() >= 2 {
+                    // now we need to get the full numbers
+                    surrounding_numbers =
+                        get_full_numbers_around_coordinates(row_idx, col_idx, &matrix);
+                    println!("Found two numbers: {:?}", surrounding_numbers);
+                    let unique_surrounding_numbers: HashSet<i128> =
+                        surrounding_numbers.iter().cloned().collect();
+                    println!(
+                        "Unique surrounding numbers: {:?}",
+                        unique_surrounding_numbers
+                    );
+                    surrounding_numbers = unique_surrounding_numbers.into_iter().collect();
+                    // println!("{}", surrounding_numbers.len());
+                    if surrounding_numbers.len() == 2 {
+                        // println!("product{}", surrounding_numbers[0] * surrounding_numbers[1]);
+                        sum_array.push(surrounding_numbers[0] * surrounding_numbers[1]);
+                        sum_value += surrounding_numbers[0] * surrounding_numbers[1];
+                    }
+                }
+            }
+        }
+    }
+    let _sum: i128 = sum_array.iter().sum();
+    println!("Sum array: {}", _sum);
+    println!("Sum value: {}", sum_value);
+}
+
+fn main() {
+    part_one();
+    part_two();
 }
